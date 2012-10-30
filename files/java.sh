d261f856fda6c8f09de04bc578dff61a343f1987
Ben Burkert <ben@benburkert.com>
2012-10-30 15:18:07 -0700

    Add java wrapper to set the fallback include path.
    Allows java to load homebrew built native libraries with the
    DYLD_FALLBACK_LIBRARY_PATH environment variable.

---
+++ files/java.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Invoke java with the $DYLD_LIBRARY_PATH set with the homebrew lib dir.
+# This allows java to load native libraries installed via homebrew.
+
+export DYLD_FALLBACK_LIBRARY_PATH="$GH_HOME/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+
+exec /usr/bin/java "$@"

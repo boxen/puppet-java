cc15e0594c46f36ebed6322f7de3b1ecf29407eb
Ben Burkert <ben@benburkert.com>
2012-10-30 17:09:38 -0700

    s/GH_HOME/BOXEN_HOME/

--- files/java.sh
+++ files/java.sh
@@ -3,6 +3,6 @@
 # Invoke java with the $DYLD_LIBRARY_PATH set with the homebrew lib dir.
 # This allows java to load native libraries installed via homebrew.
 
-export DYLD_FALLBACK_LIBRARY_PATH="$GH_HOME/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+export DYLD_FALLBACK_LIBRARY_PATH="$BOXEN_HOME/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
 exec /usr/bin/java "$@"

1fd3e35e8ab3665dd874e7abe87a3c07198d2329
James Fryman <fryman@github.com>
2013-03-07 15:17:04 -0600

    security certificate layout for drac compat

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,4 +21,24 @@ class java {
     mode    => 0755,
     require => Package['java']
   }
+
+  # Dell DRAC and Java 7 Certificate Fix
+  # http://people.binf.ku.dk/~hanne/b2evolution/blogs/index.php/2012/08/09/dell-idrac-7-will-not
+  file { [
+    "/Users/${::boxen_user}/.java",
+    "/Users/${::boxen_user}/.java/deployment",
+    "/Users/${::boxen_user}/.java/deployment/security"
+  ]:
+      ensure => directory,
+      owner  => $::boxen_user,
+      mode   => '0750';
+    "/Users/${::boxen_user}/.java/deployment/security/trusted.certs":
+      ensure => file,
+      mode   => '0640',
+      source => 'puppet:///modules/java/trusted.certs';
+    "/Users/${::boxen_user}/.java/deployment/security/trusted.jssecerts":
+      ensure => file,
+      mode   => '0640',
+      source => 'puppet:///modules/java/trusted.jssecerts';
+  }
 }

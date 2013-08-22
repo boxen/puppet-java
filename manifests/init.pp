b67da948e8b94db44e10afd88ce08cbecc2a32cc
Peter Rhoades <createdbypete@gmail.com>
2013-08-22 15:56:24 +0100

    Expanding usage comment in classes to follow convention

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,8 @@
 # Public: installs java jre-7u21
+#
+# Examples
+#
+#    include java
 class java {
   $jre_url = 'https://s3.amazonaws.com/boxen-downloads/java/jre-7u21-macosx-x64.dmg'
   $jdk_url = 'https://s3.amazonaws.com/boxen-downloads/java/jdk-7u21-macosx-x64.dmg'

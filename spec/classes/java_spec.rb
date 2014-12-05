90d41a9925852fd2af63fa66daafe1bd72db464a
Peter Souter <p.souter@kainos.com>
2014-12-05 17:12:28 +0000

    New spec to catch JDK-8027686

--- spec/classes/java_spec.rb
+++ spec/classes/java_spec.rb
@@ -32,4 +32,18 @@ describe "java" do
       :require => 'Package[java]'
     })
   end
+
+  context 'fails when java version has Yosemite relevant bug' do
+    let(:facts) { default_test_facts.merge({ :macosx_productversion_major => '10.10' }) }
+    let(:params) {
+      {
+        :update_version => '51',
+      }
+    }
+    it do
+      expect {
+        should contain_class('java')
+      }.to raise_error(/Yosemite Requires Java 7 with a patch level >= 71 \(Bug JDK\-8027686\)/)
+    end
+  end
 end

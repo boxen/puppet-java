e5ed07d7da1daf1c6a07d38f1eaf0e14188b26e1
Peter Souter <p.souter@kainos.com>
2014-12-05 13:21:54 +0000

    Add facts for Java version and patch level

---
+++ lib/facter/java_patch_level.rb
@@ -0,0 +1,23 @@
+# Fact: java_patch_level
+#
+# Purpose: get Java's patch level
+#
+# Resolution:
+#   Uses java_version fact
+#   Returns output of "java -version" and splits on the patch number (after _)
+#
+# Caveats:
+#   none
+#
+# Notes:
+#   None
+Facter.add(:java_patch_level) do
+  setcode do
+    java_version = Facter.value(:java_version)
+    if java_version.nil?
+      "NOT_INSTALLED"
+    else
+      java_patch_level = java_version.strip.split('_')[1]
+    end
+  end
+end
---
+++ lib/facter/java_version.rb
@@ -0,0 +1,23 @@
+# Fact: java_version
+#
+# Purpose: store java versions in the config DB
+#
+# Resolution:
+#   tests for presence of java;
+#   returns output of "java -version" and splits on \n + '"'
+#
+# Caveats:
+#   none
+#
+# Notes:
+#   None
+Facter.add(:java_version) do
+  setcode do
+    t_java = Facter::Util::Resolution.exec("java -version 2>&1")
+    if t_java.nil?
+      java_version = "NOT_INSTALLED"
+    else
+      j_version = t_java.to_s.lines.first.strip.split(/version/)[1].gsub(/"/, "").strip
+    end
+  end
+end

caea0b31bd09f569573069a8f144f445a6f80d1f
Peter Souter <peter.souter@puppetlabs.com>
2015-01-08 13:01:20 -0800

    Take upstream java fixes and put them here
    Got some advice from some Puppetlabs people on how to do facts better! :)

--- spec/unit/facter/java_major_version_spec.rb
+++ spec/unit/facter/java_major_version_spec.rb
@@ -5,22 +5,22 @@ describe Facter::Util::Fact do
     Facter.clear
   }
 
-  describe "java_patch_level" do
-    context 'returns java patch version extracted from java_version fact' do
+  describe "java_major_version" do
+    context 'returns java major version extracted from java_version fact' do
       before :each do
         allow(Facter.fact(:java_version)).to receive(:value).and_return("1.7.0_71")
       end
       it do
-        Facter.fact(:java_patch_level).value.should == "71"
+        Facter.fact(:java_major_version).value.should == "7"
       end
     end
 
-    context "returns nil when java_version fact not present" do
+    context 'returns nil when java_version fact not present' do
       before :each do
         allow(Facter.fact(:java_version)).to receive(:value).and_return(nil)
       end
       it do
-        Facter.fact(:java_patch_level).value.should be_nil
+        Facter.fact(:java_major_version).value.should be_nil
       end
     end
   end
